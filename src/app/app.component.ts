8e669f4f65c45a7e421bf57dff1798d9370bd326
TuNa125 <120010391+TuNa125@users.noreply.github.com>
2023-08-18 17:14:32 +0800

    changed icon for chatbot to png chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
 
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() { }
-  togglechat() { }
+  constructor() {}
 }

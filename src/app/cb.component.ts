310a4f1d18b9cbfee337ce37187a3a16e3f68faa
TuNa125 <120010391+TuNa125@users.noreply.github.com>
2023-08-18 15:14:52 +0800

    Fixed iframe loaded when button is not clicked

--- src/app/cb.component.ts
+++ src/app/cb.component.ts
@@ -1,18 +1,21 @@
-// Angular
-
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'iframe-bot',
-
   templateUrl: './cb.component.html',
-
   styleUrls: ['./cb.component.scss'],
 })
 export class IframeBotComponent {
+  iframeOpened = false; // New variable to control initial iframe state
   showChat = false;
 
   toggleChat() {
     this.showChat = !this.showChat;
+    this.iframeOpened = true; // Open the iframe when chat button is clicked
+  }
+
+  closeChat() {
+    this.showChat = false;
+    this.iframeOpened = false; // Close the iframe
   }
 }
